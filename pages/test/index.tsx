d00dcbe3ecba09046a3c22dce00398be4036fa17
John-Magne Bredal <bredal@jmmi.no>
2023-01-07 22:53:01 +0100

    Add test page

---
+++ pages/test/index.tsx
@@ -0,0 +1,31 @@
+import { Container } from "@mui/material";
+import { WindGauge } from "../../components/WindGauge";
+import { getWindDescription } from "../../utils/windspeed";
+
+// range(0, 2) => [0, 1, 2]
+// range(0, 10, 2) => [0, 2, 4, 6, 8, 10]
+function range(from: number, to: number, step = 1): number[] {
+  const _range = [];
+  for (let index = from; index <= to; index += step) {
+    _range.push(index);
+  }
+
+  return _range;
+}
+
+export default function Test() {
+
+  const gauges = range(0, 35).map(ms => {
+
+    return <div style={{ borderBottom: '1px solid black' }}>
+      <small>Windspeed {ms} {getWindDescription(ms)}</small>
+      <WindGauge windSpeed={ms} />
+    </div>
+  });
+
+  return (
+    <Container maxWidth={'sm'}>
+      {gauges}
+    </Container>
+  );
+}
