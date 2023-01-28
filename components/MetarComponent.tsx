954fb60a1f1de5a5f80ae576b8c4f98eac777231
John-Magne Bredal <bredal@jmmi.no>
2023-01-28 21:24:34 +0100

    Extract metar into separate component

---
+++ components/MetarComponent.tsx
@@ -0,0 +1,73 @@
+import { CloudOutlined, Visibility } from "@mui/icons-material";
+import { Card, Divider } from "@mui/material";
+import { Metar } from "../interfaces/metar";
+import styles from '../styles/Metar.module.css';
+import { getWindDescription } from "../utils/windspeed";
+import { TempGauge } from "./TempGauge";
+import { WindDirection } from "./WindDirection";
+import { WindGauge } from "./WindGauge";
+
+interface Props {
+  metar: Metar;
+}
+
+export function MetarComponent({ metar }: Props) {
+  const clouds = metar.clouds?.map(c => {
+    return c.code === 'CAVOK' ? 'No clouds' : `${c.text} ${c.meters ? `${c.meters} m` : ''}`
+  }).join(', ');
+  const observed = new Date(metar.observed);
+  const conditions = metar.conditions?.map(c => c.text).join(', ');
+
+  return (
+    <Card id={metar.icao} key={metar.icao} className={styles.card} variant='outlined' sx={{ backgroundColor: 'primary.main' }}>
+      <header className={styles.header}>
+        <h1 className={styles.h1}>{metar.icao}</h1>
+        <span>{metar.station.name}</span>
+      </header>
+
+      <Divider />
+
+      <div>
+        <p>Observed: {observed.toLocaleString('no')}</p>
+
+        {metar.wind &&
+          <div>
+            <div style={{ position: 'relative' }}>
+              <div style={{ width: 50, position: 'absolute', right: 0, top: -15 }}><WindDirection direction={metar.wind?.degrees} /></div>
+              <h3 className={styles.h3}>
+                Wind: {metar.wind?.speed_mps} m/s ({getWindDescription(metar.wind?.speed_mps)})
+              </h3>
+            </div>
+
+            <WindGauge windSpeed={metar.wind.speed_mps} />
+          </div>
+        }
+
+        {metar.temperature &&
+          <div>
+            <h3 className={styles.h3}>Temp: {metar.temperature?.celsius}° C</h3>
+            <TempGauge temp={metar.temperature.celsius} />
+          </div>
+        }
+
+        {metar.visibility &&
+          <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+            <Visibility />
+            <span>Visibility: {metar.visibility?.meters} meter</span>
+          </div>
+        }
+
+        {(clouds || conditions) &&
+          <div style={{ display: 'flex', alignItems: 'flex-start', gap: '10px' }}>
+            <CloudOutlined />
+            <div>
+              {clouds && <div>Clouds: {clouds}</div>}
+              {conditions && <div>Conditions: {conditions}</div>}
+            </div>
+          </div>
+        }
+
+      </div>
+    </Card>
+  );
+}
--- pages/metars.tsx
+++ pages/metars.tsx
@@ -1,14 +1,11 @@
-import { Air, CloudOutlined, Visibility } from '@mui/icons-material';
-import { Box, Card, Container, Divider, FormControl, FormLabel, Grid, MenuItem, Select, SelectChangeEvent, ToggleButton, ToggleButtonGroup } from '@mui/material';
+import { Air } from '@mui/icons-material';
+import { Box, Container, FormControl, FormLabel, Grid, MenuItem, Select, SelectChangeEvent, ToggleButton, ToggleButtonGroup } from '@mui/material';
 import { amber } from '@mui/material/colors';
 import { useEffect, useState } from 'react';
-import { TempGauge } from '../components/TempGauge';
-import { WindDirection } from '../components/WindDirection';
-import { WindGauge } from '../components/WindGauge';
+import { MetarComponent } from '../components/MetarComponent';
 import { Metar } from '../interfaces/metar';
 import styles from '../styles/Metars.module.css';
 import { getMetars } from '../utils/metar-service';
-import { getWindDescription } from '../utils/windspeed';
 
 type OrderBy = 'icao' | 'wind' | 'temp';
 
@@ -48,64 +45,7 @@ export default function Metars() {
   });
 
   // All metar cards
-  const metarElements = [...metars].sort(getSortFunction(orderBy)).map(metar => {
-    const clouds = metar.clouds?.map(c => {
-      return c.code === 'CAVOK' ? 'No clouds' : `${c.text} ${c.meters ? `${c.meters} m` : ''}`
-    }).join(', ');
-    const observed = new Date(metar.observed);
-    const conditions = metar.conditions?.map(c => c.text).join(', ');
-
-    return <Card id={metar.icao} key={metar.icao} className={styles.card} variant='outlined' sx={{ backgroundColor: 'primary.main' }}>
-      <header className={styles.header}>
-        <h1 className={styles.h1}>{metar.icao}</h1>
-        <span>{metar.station.name}</span>
-      </header>
-
-      <Divider />
-
-      <div>
-        <p>Observed: {observed.toLocaleString('no')}</p>
-
-        {metar.wind &&
-          <div>
-            <div style={{ position: 'relative' }}>
-              <div style={{ width: 50, position: 'absolute', right: 0, top: -15 }}><WindDirection direction={metar.wind?.degrees} /></div>
-              <h3 className={styles.h3}>
-                Wind: {metar.wind?.speed_mps} m/s ({getWindDescription(metar.wind?.speed_mps)})
-              </h3>
-            </div>
-
-            <WindGauge windSpeed={metar.wind.speed_mps} />
-          </div>
-        }
-
-        {metar.temperature &&
-          <div>
-            <h3 className={styles.h3}>Temp: {metar.temperature?.celsius}° C</h3>
-            <TempGauge temp={metar.temperature.celsius} />
-          </div>
-        }
-
-        {metar.visibility &&
-          <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
-            <Visibility />
-            <span>Visibility: {metar.visibility?.meters} meter</span>
-          </div>
-        }
-
-        {(clouds || conditions) &&
-          <div style={{ display: 'flex', alignItems: 'flex-start', gap: '10px' }}>
-            <CloudOutlined />
-            <div>
-              {clouds && <div>Clouds: {clouds}</div>}
-              {conditions && <div>Conditions: {conditions}</div>}
-            </div>
-          </div>
-        }
-
-      </div>
-    </Card>
-  });
+  const metarElements = [...metars].sort(getSortFunction(orderBy)).map(metar => <MetarComponent metar={metar} />);
 
   return (
     <>
