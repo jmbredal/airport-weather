7b7eae56a776b14dd56593b4519bcf081ecc323c
John-Magne Bredal <bredal@jmmi.no>
2023-02-05 21:18:19 +0100

    Add footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import { CssBaseline } from '@mui/material';
+import { Box, CssBaseline, Link } from '@mui/material';
 import { amber, green } from '@mui/material/colors';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Roboto } from '@next/font/google';
@@ -39,6 +39,12 @@ export default function App({ Component, pageProps }: AppProps) {
           <main className={roboto.className}>
             <Component {...pageProps} />
           </main>
+
+          <Box bgcolor={green[900]} padding={2} textAlign='center'>
+            <footer>
+              <Link href="mailto:bredal@jmmi.no" underline='hover' variant='body1' color={'#78dc7c'}>John-Magne Bredal</Link>
+            </footer>
+          </Box>
         </ThemeProvider>
       </React.Fragment>
     </>
